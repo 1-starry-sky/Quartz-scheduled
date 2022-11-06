91f9fd999adaf6542102763a03fd77e0df9f3ae5
1-星空 <1658916803@qq.com>
2022-11-06 19:29:48 +0800

    Initial commit

---
+++ src/main/java/com/example/config/RestTemplateConfig.java
@@ -0,0 +1,15 @@
+package com.example.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+//@Configuration
+public class RestTemplateConfig {
+    @Bean
+//    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
+        return restTemplateBuilder.build();
+    }
+}
---
+++ src/test/java/com/example/MyRedisApplicationTests.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+class MyRedisApplicationTests {
+
+	@Resource
+	private RedisTemplate redisTemplate;
+	@Test
+	void contextLoads() {
+
+		redisTemplate.opsForValue().set("myKey", "myValue");
+		System.out.println(redisTemplate.opsForValue().get("myKey"));
+	}
+
+
+}
